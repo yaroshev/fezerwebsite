7ab6fb14fb881933fe70c50cedd701aa8c135861
Yaroslav Shevchenko <yaro@Yaroslavs-MacBook-Pro.local>
2025-09-24 18:40:01 -0700

    Fix main.tsx: Remove imports for deleted files to fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { site } from './content';
-import { ErrorBoundary } from './components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>
 );
 
-if (site.name) {
-  document.title = site.name;
-}
+document.title = 'Fezer - Field Operations OS';
